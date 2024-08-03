2f2ff2c2e029173b098e3af80dfb2c5861707d45
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2024-08-03 02:57:17 +0100

    Initial commit

---
+++ src/main/java/com/ifeanyi/cinema_house/CinemaHouseApplication.java
@@ -0,0 +1,13 @@
+package com.ifeanyi.cinema_house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifeanyi/cinema_house/CinemaHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifeanyi.cinema_house;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
